ff846db0161bccd853d3066263de7a2b59c32cf8
Kelly Hays <contact@kellyhays.ca>
2015-02-14 15:10:34 -0800

    Created a new view, improved the upload page (shows image that was uploaded), improved template.

---
+++ view.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'vendor/autoload.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('template');
+$twig = new Twig_Environment($loader, array(
+    'cache' => 'compilation_cache',
+    'debug' => true,
+    'cache' => false,
+));
+$template = $twig->loadTemplate('view.html');
+
+$message = $_GET["message"];
+$target_file = $_GET["file"];
+
+
+// Pass all variables to the template
+echo $twig->render('view.html', array(
+  'title' => 'A View Page',
+  'description' => $message,
+  'image' => $target_file,
+)); 
+?>
+
